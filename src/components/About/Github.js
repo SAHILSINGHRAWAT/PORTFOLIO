d511a022fb8051a8c84ddb8e928630816110934c
sahil <sahilsinghssr1487@gmail.com>
2024-09-08 16:46:52 +0530

    ques updated

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,21 +1,29 @@
 import React from "react";
-import GitHubCalendar from "react-github-calendar";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import quesImage from "../../Assets/../Assets/ques.png";
 
 function Github() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
-      </h1>
-      <GitHubCalendar
-        username="SAHILSINGHRAWAT"
-        blockSize={15}
-        blockMargin={5}
-        color="#c084f5"
-        fontSize={16}
-      />
-    </Row>
+    <>
+      <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+          Days I <strong className="purple">Code</strong>
+        </h1>
+        {/* Responsive Image */}
+        <Col xs={15} md={10} lg={10} style={{ display: "flex", justifyContent: "center" }}>
+          <img
+            src={quesImage}
+            alt="Question"
+            className="img-fluid" // Bootstrap's responsive class
+            style={{
+              maxWidth: "100%", // Ensure it does not exceed the container width
+              height: "auto", // Maintain aspect ratio
+              padding: "10px",
+            }}
+          />
+        </Col>
+      </Row>
+    </>
   );
 }
 
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -29,16 +29,10 @@ function Techstack() {
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
@@ -48,6 +42,9 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
@@ -60,9 +57,6 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
       </Col>
     </Row>
   );
